2541e8ddd8b1379d499d7ea87eb6631a5c379e24
ntrxyz <ntri.knp@gmail.com>
2024-11-21 15:56:46 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ export const StoreContext= createContext(null);
 const StoreContextProvider =(props)=>{
 
    const [cartItems, setCartItems]=useState({});
-   const url = "http://localhost:4000"
+   const url = "https://food-del-d5q6.onrender.com"
    const [token,setToken]=useState("");
    const[food_list,setFoodList] = useState([]);
 
